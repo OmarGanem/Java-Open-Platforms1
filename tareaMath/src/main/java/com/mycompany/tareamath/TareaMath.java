675aabfe5eeb9f3d85e809461ce73b0cb0458332
OmarGanem <126487606+OmarGanem@users.noreply.github.com>
2023-11-07 15:24:10 -0600

    Add files via upload

---
+++ tareaMath/src/main/java/com/mycompany/tareamath/TareaMath.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareamath;
+
+/**
+ *
+ * @author ogane
+ * Date 01/September/2023
+ */
+public class TareaMath {
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        
+        System.out.println(Math.E);
+        
+        System.out.println(Math.cosh(-0.2));
+        
+        System.out.println(Math.decrementExact(9));
+        
+        System.out.println(Math.expm1(5));
+        
+        System.out.println(Math.floorDiv(5, 2));
+        
+        
+    }
+}
